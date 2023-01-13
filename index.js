6d23d80d524ece45e7c6f695e82441b8a15c5109
David McWhirter <david@tgsummit.com>
2023-01-12 21:21:47 -0500

    updated index with switch statements for prompts, updated seeds

--- index.js
+++ index.js
@@ -1,40 +1,111 @@
-const inquirer = require('inquirer')
+const inquirer = require("inquirer");
+const dbConnection = require("./connection");
 
-const dbPrompt = inquirer.prompt([
-  {
-    type: "choices",
-    name: "toDo",
-    message: "What is your project title?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "What is a brief description of your application?",
-  },
-  {
-    type: "input",
-    name: "install",
-    message: "Do you have any notes about installation?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Any notes on using this application?",
-  },
-  {
-    type: "checkbox",
-    name: "license",
-    message: "What licenses can apply to this application?",
-    choices: ["MIT", "Apache", "Boost", "Eclipse", "Mozilla"],
-  },
-  {
-    type: "input",
-    name: "credit",
-    message: "Is there any credit you would like to give to someone/something?",
-  },
-  {
-    type: "input",
-    name: "questions",
-    message: "Enter your Github URL.",
-  },
-]);
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        name: "question",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Create a new department",
+          "Create a new role",
+          "Add a new employee",
+          "Update employee role",
+          "Quit",
+        ],
+      },
+    ])
+    .then(function (res) {
+      switch (res.question) {
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Create a new department":
+          createDepartment();
+          break;
+        case "Create a new role":
+          createRole();
+          break;
+        case "Create a new employee":
+          createEmployee();
+          break;
+        case "Update employee role":
+          updateRole();
+          break;
+        case "Quit":
+          Quit();
+          break;
+      }
+    });
+}
+
+startPrompt();
+
+// functions to query database for viewing department role and employee tables
+
+function viewDepartments() {
+  dbConnection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startPrompt();
+  });
+}
+function viewEmployees() {
+  dbConnection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startPrompt();
+  });
+}
+function viewRoles() {
+  dbConnection.query("SELECT * FROM role", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startPrompt();
+  });
+}
+
+function createEmployee() {
+  dbConnection.query("select * from role", (err, res) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the employees first name?",
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the employees last name?",
+        },
+        {
+          type: "list",
+          name: "role-title",
+          message: "What is the new employees role?",
+          choices: res.map((role) => role.title),
+        },
+      ])
+      .then((data) => {
+        let roleTitle = res.find((role) => role.title === data.role - title);
+        dbConnection.query("insert into employee set ?", {
+          first_name: data.first_name,
+          last_name: data.last_name,
+          role_id: roleTitle.id,
+        });
+        startPrompt();
+      });
+  });
+}
--- seeds.sql
+++ seeds.sql
@@ -1,11 +1,14 @@
 INSERT INTO department (id, name)
 VALUES (001, 'Executive'),
         (002, 'Financial'),
-        (003, 'Marketing'),
-        (004, 'Human Rescources'),
-        (005, 'Management');
+        (003, 'Marketing');
 
 INSERT INTO role (title, salary, department_id)
 VALUES (CEO, 1000000, 001),
         (CFO, 800000, 002),
-        ()
+        (Merketing Director, 650000, 003);
+
+INSERT INTO employee (first_name, last_name, role_id, manger_id)
+VALUES (David, McWhirter, 001, NULL),
+        (Frank, Hoffmann, 002, 001),
+        (Trenton, Frazier, 003, 002);
